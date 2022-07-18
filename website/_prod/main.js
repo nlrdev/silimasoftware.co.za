7a9f6d54c9250eb4007cb11ddd85668e9f8a46a6
nlrdev <neale.bus@gmail.com>
2022-07-19 01:16:27 +0200

    js

--- website/_prod/main.js
+++ website/_prod/main.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'https://alpha.silimasoftware.co.za/static/particles.json', function () {
+particlesJS.load('particles-js', 'https://silimasoftware.co.za/static/particles.json', function () {
     console.log('callback - particles.js config loaded');
 });
